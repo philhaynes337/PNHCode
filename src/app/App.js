1effd0195ff0941ff2516bdca52c42efdb7394d0
philhaynes337 <71613755+philhaynes337@users.noreply.github.com>
2021-06-12 15:23:57 -0500

    init
    init

---
+++ src/app/App.js
@@ -0,0 +1,17 @@
+import Menu from '../menu/menu';
+import Header from '../header/Header';
+import Home from '../home/Home';
+import Footer from '../footer/Footer';
+
+function App() {
+  return (
+    <div>
+      <Menu />
+      <Header />
+      <Home />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
